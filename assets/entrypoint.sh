e486763ac53699e05c6188a4b9ca8fcd33b47308
Nicolas Chambers <github@tech-nic.eu>
2021-10-05 17:35:04 +0200

    Added workaround wget for RASPI4

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -4,7 +4,20 @@ set -e
 
 AWTRIX_BETA=${AWTRIX_BETA:-false}
 AUTOUPDATE=${AUTOUPDATE:-true}
-WGET_ALPINE_UPDATE=${WGET_ALPINE_UPDATE:-true}
+WGET_ALPINE_UPDATE=${WGET_ALPINE_UPDATE:-false}
+WGET_DEBIAN_UPDATE=${WGET_DEBIAN_UPDATE:-false}
+
+if [ "$WGET_ALPINE_UPDATE" = true ] ; then
+apk update
+apk add wget
+WGET_ALPINE_UPDATE=false
+fi
+
+if [ "$WGET_DEBIAN_UPDATE" = true ] ; then
+apt-get update
+apt-get install wget
+WGET_DEBIAN_UPDATE=false
+fi
 
 if [ "$AUTOUPDATE" = true ] ; then
     if [ "$AWTRIX_BETA" = true ] ; then
@@ -15,11 +28,6 @@ if [ "$AUTOUPDATE" = true ] ; then
         AWTRIX_DL_URL=https://blueforcer.de/awtrix/stable/awtrix.jar      
     fi
     
-if [ "$WGET_ALPINE_UPDATE" = true ] ; then
-apk update
-apk add wget
-WGET_ALPINE_UPDATE=false
-fi
 
 wget $AWTRIX_DL_URL -O /data/awtrix.jar
 fi
